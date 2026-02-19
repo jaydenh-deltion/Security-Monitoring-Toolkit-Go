730265e61ebd704711a2e33efcc75fd19c46655d
Jayden <97096023@st.deltion.nl>
2026-02-19 23:03:19 +0100

    Start Port Scanner in Golang

--- systems/port_scanner.go
+++ systems/port_scanner.go
@@ -1,18 +1,34 @@
 package systems
 
 import (
+	"context"
 	"fmt"
 	"net"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
+
+	"golang.org/x/sync/semaphore"
 )
 
-func ScanPort(ip string, port int) {
-	address := net.JoinHostPort(ip, fmt.Sprintf("%d", port))
-	conn, err := net.DialTimeout("tcp", address, 1*time.Second)
-	if err != nil {
-		fmt.Printf("Port is closed\n")
-		return
-	}
-	conn.Close()
-	fmt.Printf("Port %d is open\n", port)
+type PortScanner struct {
+	ip string 
+	lock *semaphore.Weighted
 }
+
+func Ulimit() int64 {
+	out, err := exec.Command("ulimit", "-n").Output()
+	if err != nil {
+		panic(err)
+	}
+	s := strings.TrimSpace(string(out))
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func Scanport(ip string, port int, timeout time.Duration) bool {
