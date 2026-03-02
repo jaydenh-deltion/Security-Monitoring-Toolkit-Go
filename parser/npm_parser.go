e5a87b2dab01965b6d672c8b3332e5f4546b32ab
Jayden <97096023@st.deltion.nl>
2026-03-02 18:37:33 +0100

    Add initial implementation of PackageJSON struct and parser package

---
+++ parser/npm_parser.go
@@ -0,0 +1,13 @@
+package parser
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type PackageJSON struct {
+	Name         string            `json:"name"`
+	Version      string            `json:"version"`
+	Dependencies map[string]string `json:"dependencies"`
+}
